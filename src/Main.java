dc6f6219671b0e7f7c910acf5d054afbf854e71f
Eetu Huovila <eetu.huovila@gmail.com>
2023-04-13 15:46:12 +0300

    Ready type practise

---
+++ src/Main.java
@@ -0,0 +1,136 @@
+import java.awt.Color;
+
+import javax.imageio.plugins.tiff.ExifTIFFTagSet;
+import javax.swing.*;
+import javax.swing.event.*;
+import java.awt.event.*;
+import java.util.concurrent.TimeUnit;
+
+import javax.swing.border.LineBorder;
+
+public class Main {
+    public static void main (String[] args) {
+        JFrame frame = new JFrame("Type practice!");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 300);
+
+        TypePracticeTool tpt = new TypePracticeTool();
+
+        JPanel panel = new JPanel();
+        JLabel wpm = new JLabel("0");
+        JLabel accuracy = new JLabel("0.0");
+
+        JTextField pool = new JTextField(65);
+        pool.setText(tpt.getCurrentCharacterPool());
+        pool.getDocument().addDocumentListener(new DocumentListener() {
+            public void changedUpdate(DocumentEvent e) {
+                setCurrentCharacterPool();
+            }
+            public void removeUpdate(DocumentEvent e) {
+                setCurrentCharacterPool();
+            }
+            public void insertUpdate(DocumentEvent e) {
+                setCurrentCharacterPool();
+            }
+        
+            public void setCurrentCharacterPool() {
+                tpt.setCurrentCharacterPool(pool.getText());
+            }
+        });
+
+        JTextField practiceArea = new JTextField(20);
+        practiceArea.getDocument().addDocumentListener(new DocumentListener() {
+            public void changedUpdate(DocumentEvent e) {
+                checkCorrectness();
+            }
+              public void removeUpdate(DocumentEvent e) {
+                checkCorrectness();
+            }
+              public void insertUpdate(DocumentEvent e) {
+                checkCorrectness();
+            }
+            
+            public void checkCorrectness() {
+                Runnable r = new Runnable() {
+
+                    @Override
+                    public void run() {
+                        boolean match = tpt.checkWord(practiceArea.getText());
+
+                        if (match) {
+                            boolean moreWords = tpt.nextWord();
+                            practiceArea.setText("");
+                            
+                            if (!moreWords) {
+                                wpm.setText(String.valueOf(tpt.getWpm()));
+                                accuracy.setText(String.format("%3.3f", tpt.getAccuracy()));
+                                panel.remove(panel.getComponentCount()-1);
+                                panel.remove(panel.getComponentCount()-1);
+                            } else {
+                                JPanel textPanel = (JPanel) panel.getComponent(panel.getComponentCount()-2);
+                                JLabel previousWord = (JLabel) textPanel.getComponent(tpt.getCurrentWord()-1);
+                                previousWord.setBorder(null);
+                                JLabel nextWord = (JLabel) textPanel.getComponent(tpt.getCurrentWord());
+                                nextWord.setBorder(new LineBorder(Color.RED, 2, true));
+                            }
+
+                            panel.revalidate();
+                            panel.repaint();
+                        }
+                    }
+                };
+                SwingUtilities.invokeLater(r);
+            }
+        });
+
+        JButton resetPool = new JButton("Reset pool");
+        resetPool.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                tpt.resetCharacterPool();
+                pool.setText(tpt.getCurrentCharacterPool());
+            }
+        });
+
+        JButton startButton = new JButton("Start!");
+        startButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                practiceArea.setText("");
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (Exception ex) {
+                    // TODO: handle exception
+                    System.exit(1);
+                } {
+
+                }
+                tpt.startPractice();
+
+                JPanel textPanel = new JPanel();
+                for (int i = 0; i < tpt.getCurrentText().length; i++) {
+                    JLabel word = new JLabel(tpt.getCurrentText()[i]);
+                    if (i == 0) {
+                        word.setBorder(new LineBorder(Color.RED, 2, true));
+                    }
+
+                    textPanel.add(word);
+                }
+
+                panel.add(textPanel);
+                panel.add(practiceArea);
+
+                panel.revalidate();
+                panel.repaint();
+                practiceArea.requestFocus();
+            }
+        });
+
+        panel.add(pool);
+        panel.add(resetPool);
+        panel.add(wpm);
+        panel.add(accuracy);
+        panel.add(startButton);
+
+        frame.getContentPane().add(panel);
+        frame.setVisible(true);
+    }
+}
---
+++ src/TypePracticeTool.java
@@ -0,0 +1,126 @@
+import java.util.Random;
+
+public class TypePracticeTool {
+    private final static String DEFAULT_CHARACTER_POOL = new String("qwertyuiopåasdfghjklöäzxcvbnmQWERTYUIOPÅASDFGHJKLÖÄZXCVBNM1234567890,.-;:_<>'*¨^´`\"!#¤%&/()=?\\~|§½@£${[]}+");
+    private String currentCharacterPool;
+    private String[] currentText;
+    private int currentWord;
+    private long startTime;
+    private long endTime;
+    private int wpm;
+    private int errors;
+    private int totalCharacters;
+    private boolean errorMode;
+    private Random rand;
+
+    public TypePracticeTool() {
+        this.currentCharacterPool = DEFAULT_CHARACTER_POOL;
+        this.rand = new Random();
+        this.errors = 0;
+        this.errorMode = false;
+    }
+
+    public void resetCharacterPool() {
+        currentCharacterPool = DEFAULT_CHARACTER_POOL;
+    }
+
+    public void startPractice() {
+        createNewText(1, 8, 15);
+        this.errors = 0;
+        this.errorMode = false;
+        this.startTime = System.currentTimeMillis();
+        System.out.println(this.totalCharacters);
+    }
+
+    public boolean checkWord(String input) {
+        boolean isStart = this.currentText[this.currentWord].startsWith(input);
+        if (!isStart && !errorMode) {
+            this.errors += 1;
+        }
+        this.errorMode = !isStart;
+
+        boolean isEqual = this.currentText[this.currentWord].equals(input);
+
+        return isEqual;
+    }
+
+    public boolean nextWord() {
+        boolean textEnd = this.currentWord >= this.currentText.length-1;
+        if (!textEnd) {
+            this.currentWord = this.currentWord +1;
+        } else {
+            this.endTime = System.currentTimeMillis();
+            this.wpm = (int) Math.round((this.currentText.length)/((this.endTime - this.startTime)/(1000.0*60)));
+        }
+
+        return !textEnd;
+    }
+
+    public boolean previousWord() {
+        boolean textStart = this.currentWord <= 0;
+        if (!textStart) {
+            this.currentWord = this.currentWord - 1;
+        }
+
+        return !textStart;
+    }
+
+    private void createNewText(int minWLength, int maxWLenght, int textLength) {
+        String[] newText = new String[textLength];
+
+        for (int i = 0; i < textLength; i++) {
+            int wordLength = rand.nextInt(maxWLenght - minWLength) + minWLength;
+            this.totalCharacters += wordLength;
+
+            String newWord = this.createNewWord(wordLength);
+            if (i != textLength-1) {
+                newWord = newWord + " ";
+                this.totalCharacters += 1;
+            }
+            newText[i] = newWord;
+        }
+
+        this.currentWord = 0;
+        this.currentText = newText;
+    }
+
+    private String createNewWord(int wordLength) {
+        int poolSize = this.currentCharacterPool.length();
+        String currentWord = "";
+
+        for (int i=0; i < wordLength; i++ ) {
+            int randomIndex = this.rand.nextInt(poolSize);
+            if (randomIndex < poolSize-1) {
+                currentWord = currentWord + this.currentCharacterPool.substring(randomIndex, randomIndex+1);
+            } else {
+                currentWord = currentWord + this.currentCharacterPool.substring(randomIndex);
+            }
+        }
+
+        return currentWord;
+    }
+
+    public double getAccuracy() {
+        return 1.0*(this.totalCharacters-this.errors)/this.totalCharacters;
+    }
+
+    public int getCurrentWord() {
+        return this.currentWord;
+    }
+
+    public String[] getCurrentText() {
+        return this.currentText;
+    }
+
+    public int getWpm() {
+        return wpm;
+    }
+
+    public String getCurrentCharacterPool() {
+        return currentCharacterPool;
+    }
+
+    public void setCurrentCharacterPool(String currentCharacterPool) {
+        this.currentCharacterPool = currentCharacterPool;
+    }
+}
